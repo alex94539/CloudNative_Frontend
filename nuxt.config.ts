4bd8f51c057904232c4fef6b1e22dfe9fb4147eb
Howard Guo <50100922+toto6038@users.noreply.github.com>
2024-05-27 00:00:10 +0800

    feat: enable nuxt eslint module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-primevue'],
+  modules: ['nuxt-primevue',
+    '@nuxt/eslint'
+  ],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
